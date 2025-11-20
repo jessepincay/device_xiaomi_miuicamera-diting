5f5cbdf6cf452b7f653e8842fde1d904af093f3d
DqrKnzx <DavidYonatan9@hotmail.com>
2025-11-20 03:02:00 +0000

    miuicamera-diting: Rename to diting

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,9 +5,9 @@
 #
 
 # Inherit from the proprietary version
-include vendor/xiaomi/miuicamera-mondrian/BoardConfigVendor.mk
+include vendor/xiaomi/miuicamera-diting/BoardConfigVendor.mk
 
-MIUICAMERA_PATH := device/xiaomi/miuicamera-mondrian
+MIUICAMERA_PATH := device/xiaomi/miuicamera-diting
 
 # Properties
 TARGET_SYSTEM_PROP += $(MIUICAMERA_PATH)/system.prop

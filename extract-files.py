1e8fb2d926680f7a40af435e4c3917298e834608
DqrKnzx <DavidYonatan9@hotmail.com>
2025-11-20 03:02:00 +0000

    miuicamera-diting: Rename to diting

--- extract-files.py
+++ extract-files.py
@@ -18,7 +18,7 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
-    'device/xiaomi/miuicamera-mondrian',
+    'device/xiaomi/miuicamera-diting',
 ]
 
 
@@ -44,7 +44,7 @@ blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
 
 module = ExtractUtilsModule(
-    'miuicamera-mondrian',
+    'miuicamera-diting',
     'xiaomi',
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
